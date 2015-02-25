83684074bf6ebe049094b891f82e635e01bde4b5
emaisne <emaisne@DE00113820.ericsson.se>
2015-02-25 13:47:18 +0100

    add interface phone

---
+++ User_Management_System/src/main/java/de/cc/phone/FeaturePhone.java
@@ -0,0 +1,11 @@
+package de.cc.phone;
+
+public class FeaturePhone implements Phone{
+
+	public int getThroughput() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+}
---
+++ User_Management_System/src/main/java/de/cc/phone/SuperSmartPhone.java
@@ -0,0 +1,10 @@
+package de.cc.phone;
+
+public class SuperSmartPhone implements Phone {
+
+	public int getThroughput() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
