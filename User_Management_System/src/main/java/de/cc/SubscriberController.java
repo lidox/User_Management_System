222633253b7b1f2a107ba07afe6572de59324d1f
ewilzwe <ewilzwe@DE00081741.ericsson.se>
2015-02-25 16:12:45 +0100

    addSubscriber

--- User_Management_System/src/main/java/de/cc/SubscriberController.java
+++ User_Management_System/src/main/java/de/cc/SubscriberController.java
@@ -1,32 +1,107 @@
 package de.cc;
 
+import java.util.Scanner;
+
+import de.cc.contract.Budget;
+import de.cc.contract.Business;
+import de.cc.contract.Contract;
+import de.cc.contract.Premium;
+import de.cc.phone.FeaturePhone;
+import de.cc.phone.Phone;
+import de.cc.phone.SimpleSmartPhone;
+import de.cc.phone.SuperSmartPhone;
+
 public class SubscriberController {
 
 	private Controller controller;
-	
+
 	public SubscriberController(Controller controller) {
 		this.controller = controller;
 	}
+
+	public void addSubscriber() {
+		try {
+		Subscriber sub = new Subscriber();
+		Scanner scanner = new Scanner(System.in);
+		 
+		System.out.print("Subscriber Name: ");
+		sub.setName(scanner.nextLine());
+		
+		System.out.print("Subscriber MSIN: ");
+		sub.setId(scanner.nextLine());
+		
+		System.out.println("Subscriber Terminal Type: ");
+		sub.setPhone(inputTerminalType());
+		
+		System.out.println("Subscriber Subscription Type: ");
+		sub.setContract(inputSubscriptionType());
+		
+		controller.getSubscriber().add(sub);
+		
+		scanner.close();
+		} catch(Exception e) {
+			System.out.println("Error");
+		}
+		
+		controller.printStartMenu();
+		
+	}
+
+	public Phone inputTerminalType() {
+		System.out.println("(1) FeaturePhone");
+		System.out.println("(2) SimpleSmartPhone");
+		System.out.println("(3) SuperSmartPhone");
+
+		switch (controller.readInt()) {
+		case 1:
+			return new FeaturePhone();
+		case 2:
+			return new SimpleSmartPhone();
+		case 3:
+			return new SuperSmartPhone();
+		default:
+			System.out.println("Illegal Input");
+			return this.inputTerminalType();
+		}
+	}
 	
-	public void printSubscriberManagementMenu(){
+	public Contract inputSubscriptionType() {
+		System.out.println("(1) Budget");
+		System.out.println("(2) Business");
+		System.out.println("(3) Premium");
+
+		switch (controller.readInt()) {
+		case 1:
+			return new Budget();
+		case 2:
+			return new Business();
+		case 3:
+			return new Premium();
+		default:
+			System.out.println("Illegal Input");
+			return this.inputSubscriptionType();
+		}
+	}
+	
+	public void printSubscriberManagementMenu() {
 		System.out.println("(1) Add Subscriber");
 		System.out.println("(2) Edit Subscriber");
 		System.out.println("(3) Remove Subscriber");
 		System.out.println("(4) List Subscribers");
 		System.out.println("(5) Return");
-		
+
 		switch (controller.readInt()) {
 		case 1:
-			//printSubscriberManagement();
+			addSubscriber();
 			break;
 		case 2:
-			//printSessionManagement();
+			// printSessionManagement();
 			break;
 		case 3:
-			//printInvoice();
+			// printInvoice();
 			break;
 		case 4:
-			//close();
+			// close();
 			break;
 		case 5:
 			controller.printStartMenu();
@@ -36,5 +111,5 @@ public class SubscriberController {
 			this.printSubscriberManagementMenu();
 		}
 	}
-	
+
 }
